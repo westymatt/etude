ef2ef64e098a3afed0bee8851423623166e2f6d2
Matt Granmoe <matt.granmoe@bestbuy.com>
2016-05-27 13:45:15 -0500

    Just load everything in web-mode and treat js as jsx since all my work will be in es6 now

--- init.el
+++ init.el
@@ -184,9 +184,10 @@
   :config
   (progn
     (add-to-list 'auto-mode-alist '("\\.jsx\\'" . web-mode))
+    (add-to-list 'auto-mode-alist '("\\.js\\'" . web-mode))
 	(add-to-list 'auto-mode-alist '("\\.dust\\'" . web-mode))
 	(add-to-list 'auto-mode-alist '("\\.ftl\\'" . web-mode))
-    (setq web-mode-content-types-alist '(("jsx" . "\\.jsx\\'")))
+    (setq web-mode-content-types-alist '(("jsx" . "\\.js[x]?\\'")))
 	(setq web-mode-markup-indent-offset 2)
 	(setq web-mode-code-indent-offset 2)
 	(setq-default indent-tabs-mode nil)
